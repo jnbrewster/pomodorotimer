b817a5159dea0aff3038222d8028a66df2f2735d
Joel <j.n.brewster@gmail.com>
2015-12-10 10:48:49 +0900

    added html and css files, need to rethink functions

--- javascript.js
+++ javascript.js
@@ -4,7 +4,6 @@
 //Pomodoro timer
 
 //Displays a count down timer in the middle of the page
-
 //click the clock image to stop/reset the timer
 
 //TODO: get better variable/function names
@@ -13,15 +12,14 @@ var relaxTime = 300;      // 5 minutes relax is the
 var extendedRelaxTime = 99999; // every 4th/3rd? time it is extended (TODO: fix meh)
 
 var seconds = 60;
-var tomatoTimes = 0;      // How many pommmmmmodoros done
+var tomatoTimes = 0;
 
 function secondsPassed() {
   var minutes = Math.round((seconds - 30)/60);
 
-  //get minutes and seconds
   var remainingSeconds = seconds % 60;
 
-  // display a 0 then the seconds when it's under 10 seconds
+  // display a 0 then the seconds remaining when it's under 10 seconds
   if (remainingSeconds < 10) {
     remainingSeconds = "0" + remainingSeconds;
   }
@@ -31,53 +29,43 @@ function secondsPassed() {
     relaxTimer();
     //then go to the relax timer
   }
-
   else {
-    //minus 1 second
     seconds--;
   }
 }
 
-function startPomodoro() {
-//Start pomodoroTimer timer.
-  //Set the pomodoro running to true.
-  //Set the relax running to false.
-
-//If pomodoro timer state is not running?
-  // PLAY button, the pomodoro running is true.
-  // PAUSE button, does nothing.
-  // RESET button, resets pomodoro.
-  // Update display.
-
-//If pomodoro timer state is running?
-  //  PAUSE button and the pomodoro running is false.
-  //  PLAY button, does nothing
-  // RESET button, resets pomodoro.
-  // Update display.
-
-//Any state?
-  // RESET button, resets pomodoro.
-
-//Update timer on screen to show time remaining.
-
-}
-
-
 
 function pomodoroTimer () {
-  //set seconds to 1500
-  //start it counting down
+  // Start pomodoro countdown
+
+  var pomodoroTimer = true;
+  var relaxTimer = false;
+
+  //If pomodoro timer state is false?
+
+  //If pomodoro timer state is true?
+
+  //Else
+
+  //Update timer on screen to show time remaining.
 
   // Add +1 to tomato times on completion
   tomatoTimes++;
+  //go to relaxTimer
 
 }
 
 function relaxTimer () {
-  //set seconds to 300
+  var pomodoroTimer = false;
+  var relaxTimer = true;
+
+  //set timer to 300
+  var relaxTime = 300;
+
   //start it counting down
 
-  //on completion set it back to pomodoroTimer
+  //on completion
+  //go back to pomodoroTimer
 }
 
 function clear() {
@@ -85,32 +73,13 @@ function clear() {
   //set timer back when user clicks clock
 }
 
+
 function pomodoroCounter() {
   if (tomatoTimes == 3) {
+    //set a global relax or put it inside the relax timer in an else
     relax = 600 //10 mins or something
+
   }
 }
 
-
-//On page load
-
-
-// When pomodoro = 0
-  // Change to relax timer
-    // Set the pomodoro running to true.
-    // Set the relax running to false.
-
-
-
-//Start relax timer.
-  //Set the timer relax running to true.
-  //Set the timer pomodoro running to false.
-    //click the PLAY button and the timer counts down.
-    //click the PAUSE button and the timer counts down.
-    //click the RESET button again and it resets.
-
-
-//Reset time
-
-//Reset relax
 </script>
