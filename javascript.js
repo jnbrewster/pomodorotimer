ba94daeded5314f65ca4728861f8bc8132905312
Joel <j.n.brewster@gmail.com>
2015-12-17 13:33:38 +0900

    Need to format account

--- javascript.js
+++ javascript.js
@@ -2,51 +2,26 @@
 <script>
 
 //Set default time
-var seconds = 1500;     //25min default for pomodoro
-var pomodoroTimes = 0;  //Number of times pomodoro and relax have completed
-var state = 0;          //What state are things moving in?
 
 // 0 - off
 // 1 - start
 // 2 - pomodoro
 // 3 - relax
 
-function Pomodoro (pomodoroTimer, relaxTimer) {
+//TODO: read over this
+//http://www.sitepoint.com/creating-accurate-timers-in-javascript/
+//Use system clock to give timer accuracy
 
-}
+//Maybe use something like flipclock
+//http://flipclockjs.com/
+
+//setInterval and clearInterval is the way to go try to cut the problem up into simple, smaller problems first and tackle each one before setting up an interface for it
 
 
+//draw the timer on the page
 
-function secondsPassed() {
-  var minutes = Math.floor((seconds - 30)/60);
-  var remainingSeconds = seconds % 60;
 
-  // display a 0 then the seconds remaining when it's under 10 seconds
-  function formatTime (seconds) {
-  if (remainingSeconds < 10) {
-    remainingSeconds = "0" + remainingSeconds;
-  }
-
-  if (seconds === 0) {
-    if (state === 1) {
-
-      relaxTimer();
-    }
-    else (state === 2) {
-
-      //finish it
-      //set back to default but increment pomodoroTimes++;
-    }
-  }
-  // Add +1 to tomato times on completion
-  //pomodoroTimes++;
-
-  }
-  else {
-    seconds--;
-    //display minutes:remainingSeconds counter
-  }
-}
+//get the timer to count down from 25 to 0
 
 
 </script>
